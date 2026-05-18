924dd5c92285874d9afeb60d1e2fca237052e749
Sudipto Chakrabarty Raja <skchakrabarty2020@gmail.com>
2026-05-18 17:56:22 +0600

    view & config

---
+++ config/DatabaseConnection.php
@@ -0,0 +1,27 @@
+<?php
+
+class DatabaseConnection
+{
+    public function openConnection()
+    {
+        $db_host = "localhost";
+        $db_username = "root";
+        $db_password = "";
+        $db_name = "hospital_appointment";
+
+        $connection = new mysqli(
+            $db_host,
+            $db_username,
+            $db_password,
+            $db_name
+        );
+
+        if ($connection->connect_error) {
+            die("Database connection failed: " . $connection->connect_error);
+        }
+
+        return $connection;
+    }
+}
+
+?>
